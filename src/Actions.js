11a1cb06abb3d81b805e9bcb176df8498b4d81e0
Janelle <dominja@oocl.com>
2020-10-28 16:12:18 +0800

    [ToDoList][Charlie-Janelle]  Feature   : added action for deletion and update

--- src/Actions.js
+++ src/Actions.js
@@ -15,3 +15,18 @@ export const deleteToDoItem = (id) => {
         type: "DELETE_TODO", payload: id
     };
 }
+export const updateTodoItem = (toDo) => {
+    return {
+        type: "UPDATE_TODO", payload: toDo
+    };
+}
+
+export const initTodos = todos => {
+    return {
+        type: "INIT_TODO", payload: todos
+    };
+
+
+
+    
+}
