8908309aee3ad578fd869dbd0b668b605d809c15
BORBECH <charlieborbs18@gmail.com>
2020-10-29 01:17:34 +0800

    [ToDoList][Charlie-Janelle]  refactor  : Design and Refactor NotFoundPage

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,10 +4,9 @@ class NotFound extends Component {
     render() {
         return (
             <div id="error">
-                <h1>404 - PAGE NOT FOUND!</h1>
+                <h1 id="errorFont">404 - PAGE NOT FOUND!</h1>
             </div>
         );
     }
 }
-
 export default NotFound;
