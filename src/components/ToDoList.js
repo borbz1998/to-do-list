ddb2866e7665f5174889444c04cae6de6371e66f
Janelle <dominja@oocl.com>
2020-10-28 23:14:50 +0800

    [ToDoList][Charlie-Janelle]  Feature  : added id in list

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import ToDoGeneratorContainer from '../containers/ToDoGeneratorContainer';
 import ToDoGroupContainer from '../containers/ToDoContainer';
 
+
 class ToDoList extends Component {
     render() {
         return (
-            <div>
-                <h2>To-Do-List</h2>
-                <ToDoGroupContainer />
+            <div id="todoList" >
+                <ToDoGroupContainer  />
                 <ToDoGeneratorContainer />
             </div>
         );
