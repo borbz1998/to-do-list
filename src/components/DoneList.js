fd3dc92b3530f7b76e553aa45a02336f28098f7d
BORBECH <charlieborbs18@gmail.com>
2020-10-28 23:41:51 +0800

    [ToDoList][Charlie-Janelle]  feature  : Created table for the DoneList

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import DoneListContainer from '../containers/DoneListContainer';
+import { Table } from 'antd';
 
 class DoneList extends Component {
     render() {
-        console.log(this.props.toDoItems);
-        // const doneList = this.props.doneItems.map((toDo) => 
-        //     <li key={toDo.id}> {toDo.text}</li>);
+        const columns = [
+            {
+                title: 'Done List',
+                dataIndex: 'text',
+                key: 'id',
+                render: text => <b>{text}</b>,
+            },
+            {
+                title: 'Create Date',
+                dataIndex: 'createdAt',
+                key: 'id',
+                render: text => <b>{text}</b>,
+            }];
 
-        const doneList = this.props.doneItems.map((toDo) => 
-        <li DoneListContainer key={toDo.id}>{toDo.text}</li>
-        );
+        // const doneList = this.props.doneItems.map((toDo) => 
+        // <li DoneListContainer key={toDo.id}>{toDo.text}</li>);
         return (
             <div>
                 <ul>
-                    {doneList}
+                    <Table dataSource={this.props.doneItems} columns={columns} />
                 </ul>
             </div>
         );
