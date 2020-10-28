1d6d1e00e5ac47302a04961f94f4863d5537afc5
BORBECH <charlieborbs18@gmail.com>
2020-10-28 12:57:35 +0800

    [ToDoList][Charlie-Janelle] feature: Implement PageNotFound in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 import ToDoList from './components/ToDoList';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import DoneListContainer from './containers/DoneListContainer';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+
         <BrowserRouter>
           <ul>
-            <li><Link to="/"  className="color"> Home Page </Link></li>
-            <li><Link to="/done" className="color"> Done List Page </Link></li>
+            <li><Link to="/" className="link"> Home Page </Link></li>
+            <li><Link to="/done" className="link"> Done List Page </Link></li>
           </ul>
-          <Route exact path="/" component={ToDoList}></Route>
-          <Route exact path="/done" component={DoneListContainer}></Route>
+          <Switch>
+            <Route exact path="/" component={ToDoList}></Route>
+            <Route exact path="/done" component={DoneListContainer}></Route>
+            <Route exact path="*" component={NotFound}></Route>
+          </Switch>
         </BrowserRouter>
-        </header>
+      </header>
     </div>
   );
 }
