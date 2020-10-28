0617f8de3414e8e619ab36736a3fa8bb7d742095
BORBECH <charlieborbs18@gmail.com>
2020-10-28 11:23:51 +0800

    [ToDoList][Charlie-Janelle] feature:Implement BrowserRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import ToDoList from './components/ToDoList';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ToDoList />
-      </header>
+      
+        <BrowserRouter>
+          <ul>
+            <li><Link to="/"  className="color"> Home Page </Link></li>
+            <li><Link to="/done" className="color"> Done List Page </Link></li>
+          </ul>
+          <Route exact path="/" component={ToDoList}></Route>
+        </BrowserRouter>
+        </header>
     </div>
   );
 }
